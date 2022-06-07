b4310cc3e29aa8c473276e256cece692fdf9d8b0
Papus <foal0912@gmail.com>
2022-06-07 15:02:49 +0900

    문제 저장용 모델 생성(1)

--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from rest_framework import serializers
 # Create your models here.
+
+class WrittenPart1(models.Model):
+
+class WrittenPart2(models.Model):
+
+class WrittenPart3(models.Model):
+
+class WrittenPart4(models.Model):
+
+class WrittenPart5(models.Model):
+
+class PracticalPart1(models.Model):
+
+class PracticalPart2(models.Model):
+
+class PracticalPart3(models.Model):
+
+class PracticalPart4(models.Model):
+
+class PracticalPart5(models.Model):
+
+class PracticalPart6(models.Model):
+
+class PracticalPart7(models.Model):
+
+class PracticalPart8(models.Model):
+
+class PracticalPart9(models.Model):
+
+class PracticalPart10(models.Model):
+
+class PracticalPart11(models.Model):
+
+class PracticalPart12(models.Model):
